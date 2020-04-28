1dc523e955a4dfec35f3bcb409ef0cb0fe73d5d9
Shelton <dellashelton@quickenloans.com>
2020-04-27 20:58:20 -0400

    initial

---
+++ ClientValidation/ClientValidation/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClientValidation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientValidation.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(User p)
+        {
+            //validate serverside(back-end)
+            //can use data anotation in the model class
+            //put info into database
+
+            return View("RegisterSuccess", p);
+        }
+    }
+}
